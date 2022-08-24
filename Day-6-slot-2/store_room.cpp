7f520844bb45a30f08f6c1ef1229bcca064bd09b
Eeshan Chanpura <80475016+Eeshan2001@users.noreply.github.com>
2022-08-24 17:37:20 +0530

    Update store_room.cpp

--- Day-6-slot-2/store_room.cpp
+++ Day-6-slot-2/store_room.cpp
@@ -5,29 +5,21 @@ Coded on: 24-08-2022 16:52
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n1,n2,i,j,c;
-    cin>>n1>>n2;
-    int a[n1],b[n2];
-    set<int> s;
-    for (int i=0;i<n1;i++){
-        cin>>a[i];
-        s.insert(a[i]);
+    int n ,p;
+    int mn = INT_MAX;
+    cin>>n>>p;
+    int m[n][n];
+    for (int i=0;i<n;i++)
+        for(int j = 0;j<n;j++)
+            cin>>m[i][j];
+    for(int i=0;i<n;i++)
+        sort(m[i],m[i]+n);
+    for(int i=0;i<n;i++){
+        if (n%2 != 0)
+            mn = min(mn, m[i][((n+1)/2)]);
+        else
+            mn = min(mn, m[i][((n+1)/2)-1]);
     }
-    for (int i=0;i<n2;i++){
-        cin>>b[i];
-        s.insert(b[i]);
-    }
-    vector<int> v(s.begin(),s.end());
-    c = s.size();
-    if (c%2 == 1){
-        c/=2;
-        cout<<v[c];
-    }
-    else{
-        n1=c/2;
-        n2 = n1-1;
-        float q = (v[n1]+v[n2])/2.0;
-        cout<<q;
-    }
+    cout<<mn;
     return 0;
 }
